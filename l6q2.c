5dbf2617fb712c2b460d63928cc1de4bbd68f081
pdrlcscrdr <166539206+pdrlcscrdr@users.noreply.github.com>
2024-04-12 16:17:55 -0300

    Exemplo de código com uma estrutura condicional.
    Verifica se a divisão entre dois números inteiros é exata.

---
+++ l6q2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<locale.h>
+//Verificação de divisão exata
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int n, d;
+	printf("Informe o numerador: \n");
+	scanf("%i", &n);
+	printf("Informe o denominador: \n");
+	scanf("%i", &d);
+	if(d == 0){
+		printf("Divisão indefinida.");
+	}
+	else if(n % d == 0){
+		printf("A divisão é exata.");
+	}
+	else{
+		printf("A divisão não é exata.");
+	}
+	return(0);
+}
