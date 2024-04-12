bdc632ce36aebeb2b6574db1fdca683d18f25dec
pdrlcscrdr <166539206+pdrlcscrdr@users.noreply.github.com>
2024-04-12 16:19:19 -0300

    Exemplo de código com uma estrutura condicional.
    Calculadora simples.

---
+++ l6q3.c
@@ -0,0 +1,41 @@
+#include<stdio.h> 
+#include<locale.h>
+//Calculadora simples com função SWITCH
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	float n1, n2;
+	int op;
+	printf("Digite um número.\n");
+	scanf("%f", &n1);
+	printf("Digite outro número.\n");
+	scanf("%f", &n2);
+	printf("Escolha uma operação:\n");
+	printf("1 - Adição\n");
+	printf("2 - Subtração\n");
+	printf("3 - Multiplicação\n");
+	printf("4 - Divisão\n");
+	scanf("%i", &op);
+	switch(op){
+		case 1:
+			printf("O resultado é: %f \n", n1 + n2);
+		break;
+	case 2:
+			printf("O resultado é: %f \n", n1 - n2);
+		break;
+	case 3:
+			printf("O resultado é: %f \n", n1 * n2);
+		break;
+	case 4:
+		if(n2 != 0){
+			printf("O resultado é: %f \n", n1 / n2);
+		}
+		else{
+		printf("A divisão é indefinida.\n");
+		}
+		break;
+	default:
+	printf("Operação inválida");
+	break;
+	}
+	return(0);
+}
