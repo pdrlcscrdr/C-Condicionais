7ec907a7f1b3bdd827df24dec8b1a5668c9156e0
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-13 11:58:28 -0300

    Update l6q5.c
    Agora com a opção de pedir mais coisas!

--- l6q5.c
+++ l6q5.c
@@ -1,38 +1,44 @@
 #include<stdio.h>
 #include<locale.h> 
-//Escolha de pedidos e c·lculo de preÁo (SWITCH)
+//Escolha de pedidos e c√°lculo de pre√ßo (SWITCH)
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float q;
-	int cod;
-	printf("C”DIGO DO ITEM   ESPECIFICA«√O   PRE«O UNIT¡RIO \n");
+	int cod, sn;
+	do{
+	printf("C√ìDIGO DO ITEM   ESPECIFICA√á√ÉO   PRE√áO UNIT√ÅRIO \n");
 	printf("     100        Cachorro Quente       8,00      \n");
 	printf("     101             Bauru            9,00      \n");
 	printf("     102            X-Salada         10,00      \n");
-	printf("     103           Hamb˙rguer        12,00      \n");
+	printf("     103           Hamb√∫rguer        12,00      \n");
 	printf("     104        Refrigerante(Lata)    3,00      \n");
-	printf("Digite o cÛdigo do produto desejado. \n");
+	printf("Digite o c√≥digo do produto desejado. \n");
 	scanf("%i", &cod);
 	printf("Digite a quantidade desejada. \n");
 	scanf("%f", &q);
 	switch(cod){
 		case 100:
-			printf("O valor a ser pago È: %.2f", q * 8);
+			printf("O valor a ser pago √©: %.2f", q * 8);
 			break;
 		case 101:
-			printf("O valor a ser pago È: %.2f", q * 9);
+			printf("O valor a ser pago √©: %.2f", q * 9);
 			break;
 		case 102:
-			printf("O valor a ser pago È: %.2f", q * 10);
+			printf("O valor a ser pago √©: %.2f", q * 10);
 			break;
 		case 103:
-			printf("O valor a ser pago È: %.2f", q * 12);
+			printf("O valor a ser pago √©: %.2f", q * 12);
 			break;
 		case 104:
-			printf("O valor a ser pago È: %.2f", q * 3);
+			printf("O valor a ser pago √©: %.2f", q * 3);
 			break;
 		default:
-			printf("CÛdigo inv·lido");
+			printf("C√≥digo inv√°lido");
 			}
+		printf("\nQuer mais alguma coisa?\n");
+		printf("1 - Sim.\n");
+		printf("2 - N√£o.\n");
+		scanf("%i", &sn);
+	}while(sn == 1);
 	return(0);
 }
