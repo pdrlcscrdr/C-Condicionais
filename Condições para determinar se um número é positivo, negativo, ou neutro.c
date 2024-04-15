8750b77ed919d43b475b7ea60f879edcb6ce4726
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:16:12 -0300

    Condições para determinar se um número é positivo, negativo, ou neutro.c

--- Condições para determinar se um número é positivo, negativo, ou neutro.c
+++ Condições para determinar se um número é positivo, negativo, ou neutro.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<locale.h> 
-//Escreva um programa que solicita e l� um valor inteiro e informa se ele � positivo, negativo ou neutro.
+//Programa que solicita e lê um valor inteiro e informa se ele é positivo, negativo ou neutro.
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int n;
-	printf("Digite um n�mero inteiro.\n");
+	//Solicitação de um número
+	printf("Digite um número inteiro.\n");
 	scanf("%i", &n);
+	//Condições para resposta
 	if(n > 0){
 		printf(" + \n");
 	}
