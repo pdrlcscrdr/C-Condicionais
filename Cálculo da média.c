f326a9ebfbb162f4d30f7bf705104710d9dbabfb
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:26:51 -0300

    Cálculo da média.c

--- Cálculo da média.c
+++ Cálculo da média.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<locale.h>
-//Cálculo da média de um aluno
+//CÃ¡lculo da mÃ©dia de um aluno
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float n1, n2, n3, mp, af, mf;
+	//Solicitando as notas
 	printf("Digite a n1: \n");
 	scanf("%f", &n1);
 	printf("Digite a n2: \n");
@@ -11,22 +12,21 @@ int main(){
 	printf("Digite a n3: \n");
 	scanf("%f", &n3);
 	mp = (n1 + n2 + n3) / 3;
+	//CondiÃ§Ãµes para resposta
 	if(mp >= 7.0){
-		printf("Você está aprovado com média %.1f. \n", mp);
+		printf("VocÃª estÃ¡ aprovado com mÃ©dia %.1f. \n", mp);
 	}
 	else if(mp >= 3.5 && mp < 7.0){
-		printf("Sua média parcial foi %.1f , então você precisa da nota da avaliação final.\n", mp);
-		printf("Digite a nota da avaliação final: \n");
+		printf("Sua mÃ©dia parcial foi %.1f , entÃ£o vocÃª precisa da nota da avaliaÃ§Ã£o final.\n", mp);
+		printf("Digite a nota da avaliaÃ§Ã£o final: \n");
 	    scanf("%f", &af);
 	    mf = ((6 * mp) + (4 * af)) / 10;
 	    if(mf >= 5.0){
-			printf("Você foi aprovado com média %.1f. \n", mf);
+			printf("VocÃª foi aprovado com mÃ©dia %.1f. \n", mf);
 		}
 		else{
-			printf("Você foi reprovado.\n");
+			printf("VocÃª foi reprovado.\n");
 		}
 		}
-		
-
-	return(0);
+		return(0);
 }
